26045c971b98573aef762346f70c1c51f549c515
Ananda <anandakrishnan710@gmail.com>
2025-08-17 20:47:07 +0530

    base app commit

---
+++ src/main/java/com/example/bytebuy/ByteBuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bytebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteBuyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bytebuy/ByteBuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bytebuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteBuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
